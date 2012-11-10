ce253a46d8f8bbb4184199e61e9b90bc75a6868d
Adrian Rossouw <adrian@daemon.co.za>
2012-11-10 04:33:32 +0200

    add socket.io connection. deploying to test sockets on nodejitsu.

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ var express = require('express');
 var app = express();
 
 app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/lib', express.static(__dirname + '/lib'));
 
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
-app.listen(8000);
+var server = app.listen(8000);
+
+var io = require('socket.io').listen(server);
+io.set('log level', 2);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('helloWorld', { hello: 'world' });
+});
 
 console.log('Server running at http://0.0.0.0:8000/');
