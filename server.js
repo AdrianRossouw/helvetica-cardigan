5a341ba86352ea9a3392564dfe8280416524f0f1
Adrian Rossouw <adrian@daemon.co.za>
2012-11-10 04:15:29 +0200

    make use of express and provide a path to the assets.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var express = require('express');
+
+var app = express();
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.listen(8000);
 
 console.log('Server running at http://0.0.0.0:8000/');
